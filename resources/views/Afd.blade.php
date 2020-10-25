a9ae515ac1a376402d6720f6fccff3ac305589af
Marcelo Silva <marcelo.silvae@utem.cl>
2020-10-24 23:17:57 -0300

    funciones
    se agregaron dos botones, para estado inicial y conjunto de estados finales.
    Ademas se agrego funcion para eliminar duplicancias  y una matriz de estados

--- resources/views/Afd.blade.php
+++ resources/views/Afd.blade.php
@@ -43,8 +43,16 @@
 <td width="70%"><input type="text" name="Alfabeto" placeholder="a,b,c,d,..." id="Alfabeto" > Formato: letra/num, </td><br>
 </tr>
 <tr>
+<td width="25%">Estado inicial:</td>
+<td width="70%"><input type="text" name="Inicial" placeholder="q" id="Inicial" > Formato: estado, </td><br>
+</tr>
+<tr>
 <td width="25%">Función de transición:</td>
 <td width="70%"><input type="text" name="Gama" placeholder="q,a,q;" id="Gama" > Formato: estadoentrada,alfabeto,estadosalida; </td><br>
+<tr>
+<td width="25%">Estado(s) final(es):</td>
+<td width="70%"><input type="text" name="Finales" placeholder="q1,q2,q3,q4,..." id="Finales" > Formato: estado,estado </td><br>
+</tr>
 </table>
 <button onclick="ValidarEntrada()">Ingresar automata </button>
 
@@ -55,21 +63,25 @@
 
 <script type="text/javascript">
 
-var Local_ConjuntoQ=[]
-var Local_Alfabeto=[]
-var Local_Gama=[]
+var E_inicial
+var ConjuntoQ=[]
+var Alfabeto=[]
+var Gama=[]
+var E_Finales=[]
 var node,edge,data,container,options,network
 
 function ValidarEntrada()
 {
+    E_inicial=document.getElementById("Inicial").value
     var Q=document.getElementById("Q").value
     var A=document.getElementById("Alfabeto").value
     var G=document.getElementById("Gama").value
+    var F=document.getElementById("Finales").value
 
     let val=/^([a-zA-Z|0-9]+[,]?)+[a-zA-Z0-9]/g
     let valg=/^(([a-zA-Z0-9]+[,]?)+[;]?)+[a-zA-Z0-9]/g
 
-    if(Q.length == 0 || A.length == 0 || G.length == 0)
+    if(Q.length == 0 || A.length == 0 || G.length == 0 || E_inicial.length == 0 || F.length == 0)
         alert("Hay campos en blanco")
     else
     {
@@ -83,19 +95,24 @@ function ValidarEntrada()
         }
         else
         alert("Formato ingresado no valido") */
-        Local_ConjuntoQ=Q.split(',')
-        Local_Alfabeto=A.split(',')
-        var preasig=G.split(';')
-        for(let index=0;index<preasig.length;index++)
-            Local_Gama.push(preasig[index].split(','))
         
+        ConjuntoQ=Datos_dupli(Q.split(','))
+        ConjuntoQ.splice(0,0,E_inicial)
+        ConjuntoQ=Datos_dupli(ConjuntoQ)
+        Alfabeto=Datos_dupli(A.split(','))
+        E_Finales=Datos_dupli(F.split(','))
+        Gama=_Gama(Datos_dupli(G.split(';')))
     }
-    console.log(Local_ConjuntoQ)
-    console.log(Local_Alfabeto)
-    console.log(Local_Gama)
+    console.log(ConjuntoQ)
+    console.log(Alfabeto)
+    console.log(Gama)
+    console.log(E_Finales)
+    console.log(tabla_estados(ConjuntoQ,Alfabeto,Gama))
     graph()
 
 }
+
+//Funciones de creacion y visualizacion de automatas
 
 function visestado(arreglo)
 {
@@ -111,8 +128,16 @@ function visestado(arreglo)
 function alfabetoxestado(arreglo)
 {
   var object=[]
-  arreglo.forEach(element => {object.push({from: Buscar_id(Local_ConjuntoQ,element[0]), to: Buscar_id(Local_ConjuntoQ,element[2]), label: element[1]})})
+  arreglo.forEach(element => {object.push({from: Buscar_id(ConjuntoQ,element[0]), to: Buscar_id(ConjuntoQ,element[2]), label: element[1]})})
   return object
+}
+
+function _Gama(arreglo)
+{
+  var gama=[]
+  for(let index=0;index<arreglo.length;index++)
+    gama.push(arreglo[index].split(','))
+  return gama
 }
 
 function Buscar_id(arreglo, nombre)
@@ -125,19 +150,61 @@ function Buscar_id(arreglo, nombre)
     
 }
 
+function Datos_dupli(arreglo)
+{
+  for(let index=0;index<arreglo.length;index++)
+    for(let jdex=0;jdex<arreglo.length;jdex++)
+    {
+      if(index!=jdex)
+      {
+        if(arreglo[index]==arreglo[jdex])
+          arreglo.splice(jdex,1)
+      }
+    }
+  return arreglo
+}
+
 function graph()
 {
-  console.log(alfabetoxestado(Local_Gama))
-  console.log(visestado(Local_ConjuntoQ))
-  node=new vis.DataSet(visestado(Local_ConjuntoQ))
-  edge=new vis.DataSet(alfabetoxestado(Local_Gama))
+  node=new vis.DataSet(visestado(ConjuntoQ))
+  edge=new vis.DataSet(alfabetoxestado(Gama))
   container= document.getElementById('mynetwork')
   data={nodes: node, edges: edge}
   options={edges:{arrows:{to:{enabled:true}}}}
   network= new vis.Network(container, data, options)
 }
+ 
+ //Fin funciones visualizacion y creacion de automata
 
+function tabla_estados(estados,alfabeto,Ftrans)
+{
+  var matriz=[]
+  for(let index=0;index<=estados.length;index++)
+  {
+    var columnas=[]
+    for(let jdex=0;jdex<=alfabeto.length;jdex++)
+    {
+      if(index == 0 && jdex == 0)
+        columnas.push('n')
+      else
+      {
+        if(index == 0)
+          columnas.push(alfabeto[jdex-1])
+        if(jdex == 0)
+          columnas.push(estados[index-1])
+      }
+      if(index != 0 && jdex != 0)
+        { 
+          for(let i=0;i<Ftrans.length;i++)
+            if(Ftrans[i][0] == columnas[0] && Ftrans[i][1] == matriz[0][jdex])
+              columnas.push(Ftrans[i][2])
+        }
 
+    }
+    matriz.push(columnas)
+  }
+  return matriz
+}
 
 </script>
 </body>
